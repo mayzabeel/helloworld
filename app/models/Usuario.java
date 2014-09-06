e59a849753b52375c26bac6641628fc326c9296a
mayzabeel <mayzabeel@gmail.com>
2014-09-06 15:25:08 -0300

    tentativa de interface

---
+++ app/models/Usuario.java
@@ -0,0 +1,97 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity(name = "Pessoa")
+public class Usuario implements Comparable <Usuario> {
+	
+	
+	// Gerador de Sequencia para o Id
+	// Todo Id tem que ter o GeneratedValue a não ser que ele seja setado
+	@Id
+	@SequenceGenerator(name = "PESSOA_SEQUENCE", sequenceName = "PESSOA_SEQUENCE", allocationSize = 1, initialValue = 0)
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	// Usar Id sempre Long
+	private Long id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String pass;
+	
+	// Construtor vazio para o Hibernate criar os objetos
+	public Usuario (){
+		
+	}
+	
+	public Usuario (String nome, String email){
+		this.email = email;
+		this.nome = nome;
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return true;
+	}
+
+	public int compareTo(Usuario outra) {		
+		return  (this.getNome().compareTo(outra.getNome()));
+	}
+
+	
+	
+	
+	
+}
